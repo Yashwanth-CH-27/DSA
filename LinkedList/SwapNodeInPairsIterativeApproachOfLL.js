d31e405d84333bd03fceeafe951569b5131f5762
Yash <yaswanth.chundru@gmail.com>
2026-03-30 12:12:41 +0530

    Swap Nodes in pairs Iterative Approach Of Linked List

---
+++ LinkedList/SwapNodeInPairsIterativeApproachOfLL.js
@@ -0,0 +1,16 @@
+
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+    let dummy = new ListNode()
+    dummy.next = head;
+    let prev = dummy;
+    while(prev.next && prev.next.next){
+        let first = prev.next;
+        let second = prev.next.next;
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+        prev = first
+    }
+    return dummy.next
+};
