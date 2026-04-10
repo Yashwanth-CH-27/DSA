ebfae47fa99fbedecbc444f54afc15a38aa8ca4c
Yash <yaswanth.chundru@gmail.com>
2026-04-10 11:49:27 +0530

    Finding Words Containing Char

---
+++ Strings/FindingWordsContainingChar.js
@@ -0,0 +1,11 @@
+var findWordsContaining = function(words, x) {
+    let res = []
+    for(let i = 0; i < words.length; i++){
+        if(words[i].includes(x)){
+            res.push(i)
+        }
+    }
+    return res
+};
+
+console.log(findWordsContaining(["hellow","world"], "o"))
