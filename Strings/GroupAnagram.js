d1002a30b4f2b2ba2ab94d2e09669e9a92380b10
Yash <yaswanth.chundru@gmail.com>
2026-05-23 11:17:06 +0530

    Group Anagram of Strings Approach-2

--- Strings/GroupAnagram.js
+++ Strings/GroupAnagram.js
@@ -52,4 +52,25 @@ var groupAnagrams = function(strs) {
     }
     return [...map.values()];
 };
+//Approach-2
+var groupAnagrams = function(strs) {
+    let map = new Map()
+    for(let i = 0; i < strs.length; i++ ){
+        let freqArr = Array(26).fill(0)
+        let s = strs[i]
+        for(let j = 0; j < s.length; j++){
+            let index = s[j].charCodeAt() - 'a'.charCodeAt()
+            freqArr[index]++
+        }
+        let key = freqArr.join("#")
+        if(!map.has(key)){
+            map.set(key, [s])
+        }
+        else{
+            map.get(key).push(s)
+        }
+    }
+    return [...map.values()]
+};
+
 console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
