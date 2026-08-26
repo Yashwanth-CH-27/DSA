a71a7f5a84de8534b504bd976f5187231eb51969
Yash <yaswanth.chundru@gmail.com>
2026-08-26 18:44:45 +0530

    First and Last Occurence of a sorted array using binary search

---
+++ BinarySearch/FristandLastPosition.py
@@ -0,0 +1,64 @@
+# 34. Find First and Last Position of Element in Sorted Array
+
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+
+def searchRange(nums, target):
+    first = -1
+    last = -1
+
+    #first occurence
+    l = 0
+    r = len(nums) - 1
+    while(l <= r):
+        m = (l + r) // 2
+        if(nums[m] == target):
+            first = m
+            r = m - 1
+        elif(target > nums[m]):
+            l = m + 1
+        else:
+            r = m - 1
+    
+    #last occurence
+    l = 0
+    r = len(nums) - 1
+    while(l <= r):
+        m = (l + r) // 2
+        if(nums[m] == target):
+            last = m
+            l = m + 1
+        elif(target > nums[m]):
+            l = m + 1
+        else:
+            r = m - 1
+    return [first,last]
+
+print(searchRange([2,4,4,4,6,10],4))
